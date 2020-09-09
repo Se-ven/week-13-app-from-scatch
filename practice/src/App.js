caee4beceb6ca178b5031f83f4d1202fff3e3dda
Nick Lavery <tuneboynick@gmail.com>
2020-09-09 16:44:22 -0400

    Basic frontend materials and framework

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './logo.png';
 import './App.css';
+import 'reactstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {
+  Container,
+  Nav, NavItem,
+  Dropdown, DropdownItem,
+  DropdownToggle, DropdownMenu,
+  NavLink
+} from 'reactstrap';
+import Diets from "./components/Diets";
+import Foods from "./components/Foods";
+import WorkoutJournal from "./components/WorkoutJournal";
+import Workouts from "./components/Workouts";
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Container>
+      <div className="App">
+        <header className="App-body">
+          <img src={logo} className="App-logo" alt="logo" />
+          <br />
+
+          <p>
+            Getting big, little by little.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://github.com/se-ven/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            --Nick Lavery--
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </Container>
   );
 }
 
-export default App;
+function Primary(props) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
+  return (
+    <Router>
+      <div>
+        <h2 className="App-header">
+          Nerd Gainer
+      </h2>
+
+<Nav tabs>
+          <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle nav caret>
+              Diet Possibilities
+    </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Primal</DropdownItem>
+              <DropdownItem>Keto</DropdownItem>
+              <DropdownItem>Cyclical Keto</DropdownItem>
+              <DropdownItem>Vegan</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <NavItem>
+            <NavLink href="/Foods">Food Grid</NavLink>
+          </NavItem>
+
+          <Dropdown nav is Open={dropdownOpen} toggle={toggle}>
+            <DropdownToggle nav caret>
+              Workout Approaches
+</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Progressive Overload</DropdownItem>
+              <DropdownItem>HIT</DropdownItem>
+              <DropdownItem>TSC</DropdownItem>
+              <DropdownItem>Negatives</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+
+          <NavItem>
+            <NavLink href="/WorkoutJournal">Workout Journal</NavLink>
+          </NavItem>
+
+        </Nav>
+
+
+      </div>
+
+
+
+
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/Diets">
+          <Diets />
+        </Route>
+        <Route path="/Foods">
+          <Foods />
+        </Route>
+        <Route path="/WorkoutJournal">
+          <WorkoutJournal />
+        </Route>
+        <Route path="/Workouts">
+          <Workouts />
+        </Route>
+      </Switch>
+    </Router>
+
+  );
+}
+
+export default {
+  App,
+  Primary
+};
